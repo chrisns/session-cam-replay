090f69343fc579416c979dd0a0e110f1184cf8eb
morrislaptop <craig.michael.morris@gmail.com>
2014-11-04 15:08:08 +0000

    Hook into the session end modal so we can go to the next session - works however the session end modal is displayed like 10 times and throttling / debouncing doesn't work

--- client/app/player/player.directive.js
+++ client/app/player/player.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('sessionCamReplayApp')
-  .directive('player', function ($sce, $timeout, $log) {
+  .directive('player', function ($sce, $timeout, $log, $window) {
     return {
       templateUrl: 'app/player/player.html',
       restrict: 'EA',
@@ -10,14 +10,47 @@ angular.module('sessionCamReplayApp')
       },
       link: function (scope, element, attrs) {
 
+        var session;
         function loadSession() {
-          var session = scope.sessions[Math.floor(Math.random() * scope.sessions.length)];
-          if ( session ) {
-            $log.info('Loading ' + session.url + ' for ' + session.duration + ' seconds');
-            scope.currentProjectUrl = $sce.trustAsResourceUrl(session.url);
-            $timeout(loadSession, session.duration);
-          }
+          $log.info('loadSession()');
+
+          // Get a random session that isn't the current one
+          do {
+            var randomSession = scope.sessions[Math.floor(Math.random() * scope.sessions.length)];
+          } while (randomSession == session);
+
+
+          session = randomSession;
+          $log.info('Loading ' + session.url + ' for ' + session.duration + ' seconds');
+          scope.currentProjectUrl = $sce.trustAsResourceUrl(session.url);
+          scope.$apply();
         }
+
+        $window.sessionCamPlayer = {
+          frameLoaded: function () {
+            var p = $window.frames[0].sessionCamPlayer;
+
+            // Run the original function but return it later..
+            var result = p.frameLoaded();
+
+            // Auto play
+            $window.frames[0].sessionCamPlayer.play();
+
+            // Hook into when the playback is finished.
+            var _showSessionPlaybackFinished = p.showSessionPlaybackFinished;
+            var throttled = _.debounce(loadSession, 10000, {
+              leading: true
+            });
+            p.showSessionPlaybackFinished = function () {
+              $log.info('showSessionPlaybackFinished()');
+              throttled();
+              _showSessionPlaybackFinished();
+            };
+
+            // Return original method
+            return result;
+          }
+        };
 
         // When we get sessions start the randomisation
         var sessionWatch = scope.$watch('sessions', function (newValue) {
